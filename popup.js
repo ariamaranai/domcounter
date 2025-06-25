8eda0018c74406739dedc683f37a78d22bb0ca49
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 22:20:47 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,48 +1,11 @@
-chrome.tabs.query({ active: !0, currentWindow: !0 }, tab =>
-  chrome.userScripts.execute({
-    target: { tabId: tab[0].id },
-    js: [{ code:
-`(() => {
-  let x = document;
-  let walker = x.createTreeWalker(x.documentElement, 133);
-  let elementCounter = 0;
-  let textCounter = 0;
-  let commentCounter = 0;
-  let tagCounter = {};
-  let { currentNode } = walker;
-
-  while (
-    currentNode = walker.nextNode(
-      (x = currentNode.nodeType) == 1
-        ? (tagCounter[x = currentNode.tagName.toLowerCase()] ??= 0, ++tagCounter[x], ++elementCounter)
-        : x == 3
-          ? ++textCounter
-          : ++commentCounter
-    )
-  );
-
-  let left = "";
-  let right =
-    (elementCounter + textCounter + commentCounter) + "\\n" +
-    elementCounter + "\\n" +
-    textCounter + "\\n" +
-    commentCounter + "\\n\\n";
-  let entries = Object.entries(tagCounter).sort((a, b) => a[1] < b[1] || -1);
-  let i = 0;
-
-  while (
-    left += (x = entries[i])[0] + "\\n",
-    right += x[1] + "\\n",
-    ++i < entries.length
-  );
-  return [left += "\\n ", right];
-})();`
-    }]
-  }).then(results => {
-    if (results &&= results[0].result) {
-      let { body } = document;
-      body.firstChild.textContent += results[0];
-      body.lastChild.textContent += results[1];
-    }
-  }).catch(() => 0)
-);
+chrome.tabs.query({ active: !0, currentWindow: !0 }, async tab => {
+  try {
+    let { result } = (await chrome.userScripts.execute({
+      target: { tabId: tab[0].id },
+      js: [{ file: "main.js" }]
+    }))[0];
+    let { body } = document;
+    body.firstChild.textContent += result[0];
+    body.lastChild.textContent += result[1];
+  } catch (e) {}
+});
