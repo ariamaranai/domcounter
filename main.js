3b4e6574b3dbed467935f91a268346cb7448c620
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-29 23:42:14 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 (() => {
-  let x = document;
-  let walker = x.createTreeWalker(x.documentElement, 133);
+  let walker = document.createTreeWalker(document.documentElement, 133);
   let elementCounter = 0;
   let textCounter = 0;
   let commentCounter = 0;
   let tagCounter = {};
-  let { currentNode } = walker;
-
-  while (
-    currentNode = walker.nextNode(
-      (x = currentNode.nodeType) == 1
-        ? (tagCounter[x = currentNode.tagName.toLowerCase()] ??= 0, ++tagCounter[x], ++elementCounter)
-        : x == 3
-          ? ++textCounter
-          : ++commentCounter
-    )
-  );
-
+  let node = walker.currentNode;
+  while (node) {
+    let type = node.nodeType;
+    if (type == 1) {
+      let tagName = node.tagName.toLowerCase();
+      tagCounter[tagName] ??= 0;
+      ++tagCounter[tagName];
+      ++elementCounter;
+    } else
+      type == 3 ? ++textCounter : ++commentCounter;
+    node = walker.nextNode();
+  }
   let left = "";
-  let right =
-    (elementCounter + textCounter + commentCounter) + "\n" +
-    elementCounter + "\n" +
-    textCounter + "\n" +
-    commentCounter + "\n\n";
-  let entries = Object.entries(tagCounter).sort((a, b) => a[1] < b[1] || -1);
+  let right = (elementCounter + textCounter + commentCounter) + "\n" + elementCounter + "\n" + textCounter + "\n" + commentCounter + "\n\n";
+  let entries = Object.entries(tagCounter).sort((a, b) => a[1] < b[1] ? 1 : -1);
   let i = 0;
-
+  let x;
   while (
     left += (x = entries[i])[0] + "\n",
     right += x[1] + "\n",
     ++i < entries.length
   );
+  
   return [left += "\n ", right];
 })();
