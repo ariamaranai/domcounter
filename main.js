91be8136b576894504ad0ca891ccb0727a2ba08a
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-29 23:43:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -16,16 +16,16 @@
       type == 3 ? ++textCounter : ++commentCounter;
     node = walker.nextNode();
   }
+
   let left = "";
   let right = (elementCounter + textCounter + commentCounter) + "\n" + elementCounter + "\n" + textCounter + "\n" + commentCounter + "\n\n";
   let entries = Object.entries(tagCounter).sort((a, b) => a[1] < b[1] ? 1 : -1);
   let i = 0;
-  let x;
   while (
-    left += (x = entries[i])[0] + "\n",
-    right += x[1] + "\n",
+    left += (node = entries[i])[0] + "\n",
+    right += node[1] + "\n",
     ++i < entries.length
   );
-  
+
   return [left += "\n ", right];
 })();
