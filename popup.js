c0a019b93d0a86b1a231e3261e4e41cbf82df975
a <106607480+ariamaranai@users.noreply.github.com>
2026-02-05 12:31:38 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 chrome.tabs.query({ active: !0, currentWindow: !0 }, async tab => {
   try {
+    let d = document;
     let { result } = (await chrome.userScripts.execute({
       target: { tabId: tab[0].id },
       js: [{ file: "main.js" }]
     }))[0];
-    let { body } = document;
-    body.firstChild.textContent += result[0];
-    body.lastChild.textContent += result[1];
+    let i = 0;
+    while (
+      d.body.appendChild(d.createElement("p")).textContent = result[i][0] + "\n" + result[i][1],
+      ++i < result.length
+    );
   } catch {}
 });
